aa863578f59510ae5320f55e6596a46f0436fd7d
Reem <apple@Apples-MacBook-Pro.local>
2022-08-23 01:30:59 +0600

    one part done

---
+++ js/select.js
@@ -0,0 +1,6 @@
+buttonClicked('selectButton1', 'player1');
+buttonClicked('selectButton2', 'player2');
+buttonClicked('selectButton3', 'player3');
+buttonClicked('selectButton4', 'player4');
+buttonClicked('selectButton5', 'player5');
+buttonClicked('selectButton6', 'player6');
